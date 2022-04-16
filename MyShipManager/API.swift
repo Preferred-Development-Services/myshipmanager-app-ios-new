d19d917f3fc1e40a2ac3eafc696ba0dce7000002
Heath Peeples <43253254+heathpeeples@users.noreply.github.com>
2022-04-16 16:09:14 -0500

    added check for when session has expired.  appending un/pw to get and post requests

--- MyShipManager/API.swift
+++ MyShipManager/API.swift
@@ -155,7 +155,11 @@ class API: ObservableObject {
         }
 
     func get(proc: String) -> URLRequest? {
-        guard let url = URL(string: base + proc) else { return nil }
+        let urlUser = user.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+       let urlPass = pass.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        
+        guard let url = URL(string: base + proc + "?email=\(urlUser)&password=\(urlPass)&remember=0&app=1") else { return nil }
+//        guard let url = URL(string: base + proc) else { return nil }
 
         var req = URLRequest(url: url)
         req.httpMethod = "GET"
@@ -169,7 +173,10 @@ class API: ObservableObject {
     }
     
     func post(proc: String, bodyStr: String) -> URLRequest? {
-        guard let url = URL(string: base + proc) else { return nil }
+        let urlUser = user.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        let urlPass = pass.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        
+        guard let url = URL(string: base + proc + "?email=\(urlUser)&password=\(urlPass)&remember=0&app=1") else { return nil }
         var req = URLRequest(url: url)
         req.httpMethod = "POST"
         req.setValue("application/json", forHTTPHeaderField: "Accept")
